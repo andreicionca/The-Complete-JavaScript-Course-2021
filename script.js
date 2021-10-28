fea41778964c2383d002a5a72d5a9e00c4679f17
Andrei Cionca <andreicionca1@gmail.com>
2021-10-28 15:37:11 +0300

    ternary operator

--- script.js
+++ script.js
@@ -4,44 +4,9 @@ JavaScript Fundamentals Part 1
 
 */
 
-// - Switch Statement - //
+// - Conditional Operator: Ternary Operator - //
+// conditia `a<b` apoi urmeaza operatorul `?` urmat de rezultat pentru TRUE apoi operatorul `:` urmat de rezultat pentru FALSE → a<b ? `DA` : `NU`;
+// Exemplu ↓
 
-const day = "friday";
-
-switch (day) {
-  case "monday": // day === 'monday'
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Record videos");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D");
-    break;
-  default:
-    console.log("Not a valid day!");
-}
-
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("Enjoy the weekend :D");
-} else {
-  console.log("Not a valid day!");
-}
+const age = 18;
+console.log(`I like to drink ${age >= 19 ? "wine 🎶" : "water 😂"}`);
