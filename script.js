45a7da34b1e461bb659f221fa28812290eb97511
Andrei Cionca <andreicionca1@gmail.com>
2021-10-28 18:30:28 +0300

    solving Coding Challenge #2

--- script.js
+++ script.js
@@ -5,44 +5,35 @@ JavaScript Fundamentals Part 2
 
 */
 
-// - Array Operations (Methods) - //
+// Coding Challenge #2
 
-// Push
-//adauga un element la sfarsitul sirului
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
 
-const friends = [`Daniel`, `Matei`, `Alex`];
-const newLenght = friends.push(`Mihai`);
-console.log(newLenght);
-console.log(friends[friends.length - 1]);
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
 
-// Unshift
-//adauga un element la inceputul sirului
+TEST DATA: 125, 555 and 44
 
-friends.unshift("David");
-console.log(friends);
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
 
-//Pop
-//elimina ultimul element din sir
+GOOD LUCK 😀*/
 
-friends.pop();
-const popped = friends.pop();
-console.log(popped);
-console.log(friends);
+const calcTip = (billValue) =>
+  billValue <= 300 && billValue >= 50 ? billValue * 0.15 : billValue * 0.2;
 
-//Shift
-//elimina primul element dintr-un sir
+const bills = [125, 555, 44];
 
-friends.shift();
-console.log(friends);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
-//IndexOf
-//indica pozitia unui element intr-un sir
-console.log(friends.indexOf(`Daniel`));
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-//Includes
-//returneaza True sau False daca cautam un element din sir;
+console.log(tips[0]);
+console.log(tips[1]);
+console.log(tips[2]);
 
-console.log(friends.includes(`Matei`));
-if (friends.includes(`Matei`)) {
-  console.log(`You have a friend called Matei`);
-}
+console.log(total[0]);
+console.log(total[1]);
+console.log(total[2]);
