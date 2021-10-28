0c176ffcc6b721edf112287dcb74c865ecf241ce
Andrei Cionca <andreicionca1@gmail.com>
2021-10-28 14:04:36 +0300

    solving Coding Challenge #1

--- script.js
+++ script.js
@@ -3,39 +3,28 @@
 JavaScript Fundamentals Part 1
 
 */
+// - Coding Challenge #1 - //
 
-// - Basic Operators - //
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
 
-// Math operators
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 
-// Ridicarea la PUTERE
-// 2 la puterea 3 se scrie → 2**3;
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
 
-// Adunare STRING sau Concatenare
+GOOD LUCK 😀
+*/
 
-const firstName = "Andrei";
-const lastName = "Cionca";
-console.log(firstName + " " + lastName);
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
 
-const strIng = "I'm " + firstName + ", and my last name is " + lastName;
-console.log(strIng);
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
 
-// Alte operatii intr-o variabila
-// let x = 10 + 5 → mai intai se face adunarea (10+5) si apoi se atribuie rezultatul (15) variabilei x;
-// Expresia x += 10 inseamna x = x + 10;
-// Expresia x++ inseamna x = x + 1;
-// Assignment variable = right-to-left; Exemplu ↓
-let x, y;
-x = y = 35 - 10 - 5;
-console.log(x, y);
-
-// IN cazul de mai sus mai intai se va face scaderea deci vom avea x = y = 20, apoi se atribuie valoarea 20 variabilelor incepand din dreapta spre stanga (right-to-left) astfel ca mai intai y = 20 iar apoi x = 20 (pentru mai multe detalii vezi pe net Operators precedence https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence);
-
-// Operatori de comparare
-// const num1 = 10>=9; → in acest caz variabila 'num1' va fi de tipul bool si va avea valoarea TRUE
-// console.log (2021-1991 < 2021- 2018); → in acest caz mai intai se vor face scaderile si apoi se vor compara rezultatele (pentru mai multe detalii vezi pe net Operators precedence https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence)
+console.log(BMIMark, BMIJohn, markHigherBMI);
