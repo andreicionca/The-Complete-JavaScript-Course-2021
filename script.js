55774279b04d8d30e6cb5487194a0528679e4cca
Andrei Cionca <andreicionca1@gmail.com>
2021-10-30 22:28:00 +0300

    loop backwards and loops in loops

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ JavaScript Fundamentals Part 2
 
 */
 
-// - For Loop- //
-
-for (let i = 1; i <= 30; i++) {
-  console.log(`Lifting weights repetition ${i}`);
-}
+// - Loop Backwards and Loops in Loops- //
 
 const andreiArray = [
   `Andrei`,
@@ -20,37 +16,14 @@ const andreiArray = [
   true,
 ];
 
-const types = [];
-for (let i = 0; i < andreiArray.length; i++) {
-  console.log(andreiArray[i], typeof andreiArray[i]);
-
-  // Filling types array
-  //types[i] = typeof andreiArray[i]
-  types.push(typeof andreiArray[i]);
-}
-console.log(types);
-
-const years = [1991, 2000, 2009, 1937];
-
-const ages = [];
-
-for (let i = 0; i < years.length; i++) {
-  ages.push(2021 - years[i]);
-  console.log(ages[i]);
+for (let i = andreiArray.length - 1; i >= 0; i--) {
+  console.log(andreiArray[i]);
 }
 
-// continue and break
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------------Starting exercises ${exercise}`);
 
-for (let i = 0; i < andreiArray.length; i++) {
-  //continue is to exit the current iteration of the loop and continue to the next one
-  if (typeof andreiArray[i] !== `string`) continue;
-
-  console.log(andreiArray[i], typeof andreiArray[i]);
-}
-
-for (let i = 0; i < andreiArray.length; i++) {
-  //break is used to completely terminate the whole loop.
-  if (typeof andreiArray[i] === `number`) break;
-
-  console.log(andreiArray[i], typeof andreiArray[i]);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lifting weight repetition ${rep}`);
+  }
 }
