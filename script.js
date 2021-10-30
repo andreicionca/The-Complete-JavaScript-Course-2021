56bee54e0938067ea1fe80089fda514e107fd53f
Andrei Cionca <andreicionca1@gmail.com>
2021-10-30 18:19:58 +0300

    objects introduction

--- script.js
+++ script.js
@@ -5,35 +5,60 @@ JavaScript Fundamentals Part 2
 
 */
 
-// Coding Challenge #2
+// - Objects introduction- //
 
-/*
-Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+//Array
 
-1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
-2. And now let's use arrays! So create an array 'bills' containing the test data below.
-3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
-4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+const andreiArray = [
+  `Andrei`,
+  `Cionca`,
+  2021 - 1991,
+  `teacher`,
+  [`Marian`, `Simion`, `Iulian`],
+];
 
-TEST DATA: 125, 555 and 44
+//Object
 
-HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+const andrei = {
+  firstName: `Andrei`,
+  lastName: `Cionca`,
+  age: 2021 - 1991,
+  job: `teacher`,
+  friends: [`Marian`, `Iuga`, `Iulian`],
+};
 
-GOOD LUCK 😀*/
+// Dot and Bracket notation
 
-const calcTip = (billValue) =>
-  billValue <= 300 && billValue >= 50 ? billValue * 0.15 : billValue * 0.2;
+console.log(andrei.firstName);
 
-const bills = [125, 555, 44];
+console.log(andrei[`lastName`]);
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const nameKey = `Name`;
+console.log(andrei[`first${nameKey}`]);
+console.log(andrei[`last` + nameKey]);
 
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+const interstedIn = prompt(
+  `What do you want to know about Andrei? Choose between firstName, lastName, age, job, and friends`
+);
 
-console.log(tips[0]);
-console.log(tips[1]);
-console.log(tips[2]);
+//Add new proprietes to object
 
-console.log(total[0]);
-console.log(total[1]);
-console.log(total[2]);
+andrei.location = `Romania`;
+andrei[`twitter`] = `andreicionca1`;
+
+if (andrei[interstedIn]) {
+  console.log(andrei[interstedIn]);
+} else {
+  console.log(`Wrong choice`);
+}
+
+//Challenge
+
+console.log(
+  `${andrei.firstName} are ${andrei.friends.length} prieteni iar cel mai bun prieten al lui este ${andrei.friends[0]}`
+);
+console.log(
+  `${andrei.firstName} are ${
+    andrei.friends.length
+  } prieteni iar cel mai bun prieten al lui este ${andrei[`friends`][0]}`
+);
