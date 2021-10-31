7a33e0b598cb14f954f640d35a8626560c12948a
Andrei Cionca <andreicionca1@gmail.com>
2021-10-31 22:54:10 +0200

    hide secret number

--- project-guess-my-number/script.js
+++ project-guess-my-number/script.js
@@ -3,7 +3,6 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -11,6 +10,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
