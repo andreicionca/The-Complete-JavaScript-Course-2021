1ba5cb3b46f25cb80e6c90464c831e3e038dfffc
Andrei Cionca <andreicionca1@gmail.com>
2021-10-31 21:06:02 +0200

    work with querySelector for manipulating elements

--- script.js
+++ script.js
@@ -4,3 +4,10 @@
 JavaScript in the Browser: DOM and Events Fundamentals
 
 */
+
+// - Selecting and Manipulating Elements - //
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'JavaScript is fun';
+document.querySelector('.field').value = 1000000;
+console.log(document.querySelector('.field').value);
