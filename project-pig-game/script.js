0caafa7266409c1d8cbc811c5b653e25f00952f5
Andrei Cionca <andreicionca1@gmail.com>
2021-11-01 22:46:46 +0200

    switching the active player

--- project-pig-game/script.js
+++ project-pig-game/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -13,7 +14,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   //generating random dice
@@ -27,8 +30,16 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //display score on player1
     currentScore += dice;
-    currentScore0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+
+    //toggle will add the class if it is not there and if it is there, it will remove it
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
