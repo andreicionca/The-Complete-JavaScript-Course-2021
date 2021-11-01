5c8181dbedafd4edc3cfacddfeaed55e11bb5892
Andrei Cionca <andreicionca1@gmail.com>
2021-11-01 19:45:40 +0200

    store querySelectors in variables

--- project-modal-window/script.js
+++ project-modal-window/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+//whenever we use query selector with a selector, which actually matches multiple elements, only the first one will get selected. And so here, we need query selectorAll.
+
+//this NodeList here is a little bit like an array. It's not exactly like an array. But here for now, it works as though it was an array.
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
