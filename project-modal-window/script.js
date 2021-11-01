6ffa8fec6c28313e74c219f54b40856b5219a936
Andrei Cionca <andreicionca1@gmail.com>
2021-11-01 20:09:58 +0200

    make functions for open and close modal window

--- project-modal-window/script.js
+++ project-modal-window/script.js
@@ -9,20 +9,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 //this NodeList here is a little bit like an array. It's not exactly like an array. But here for now, it works as though it was an array.
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
