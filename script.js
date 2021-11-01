3159f0572a252f30e0e6f91064d9075e13f8ea83
Andrei Cionca <andreicionca1@gmail.com>
2021-11-01 19:19:02 +0200

    make function for aces elements

--- script.js
+++ script.js
@@ -22,3 +22,11 @@ document.querySelector('.field').value = 1000000;
 document.querySelector('.btn').addEventListener('click', function () {
   document.querySelector('.field').value = 0;
 });
+
+// best practice with elements is to select the elements that we need, and then we store the selections into variables. And then we can use them over and over again.
+//document.querySelector('.message').textContent = 'JavaScript is fun';
+
+const messageContent = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+messageContent('I made some changes');
