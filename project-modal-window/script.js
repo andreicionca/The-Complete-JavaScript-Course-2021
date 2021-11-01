64d85f3afe9ef2331b66fcaa2031a5da0a9db20b
Andrei Cionca <andreicionca1@gmail.com>
2021-11-01 20:32:27 +0200

    set key press event

--- project-modal-window/script.js
+++ project-modal-window/script.js
@@ -15,8 +15,8 @@ const openModal = function () {
 };
 
 const closeModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
@@ -25,3 +25,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
