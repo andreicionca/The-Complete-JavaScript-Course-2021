f996a6544fd8c6f05cf501fd19a79d5d0737b3cd
Andrei Cionca <andreicionca1@gmail.com>
2021-11-01 21:59:57 +0200

    set rolling the dice

--- project-pig-game/script.js
+++ project-pig-game/script.js
@@ -2,6 +2,33 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const currentScore0El = document.querySelector('#current--0');
+const currentScore1El = document.querySelector('#current--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  //generating random dice
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //check for rolled dice 1
+  if (dice !== 1) {
+    //display score on player1
+    currentScore += dice;
+    currentScore0El.textContent = currentScore;
+  } else {
+    //switch to next player
+  }
+});
