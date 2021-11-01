b8b56c511dc39385a1f25f57f88748994fc31810
Andrei Cionca <andreicionca1@gmail.com>
2021-11-01 20:06:01 +0200

    set buttons to open and close modal window

--- project-modal-window/script.js
+++ project-modal-window/script.js
@@ -9,3 +9,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 //this NodeList here is a little bit like an array. It's not exactly like an array. But here for now, it works as though it was an array.
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
