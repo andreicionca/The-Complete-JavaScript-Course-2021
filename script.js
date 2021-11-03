d9853e9b0466e7efade4c9e1ed7163b3f3f20d34
Andrei Cionca <andreicionca1@gmail.com>
2021-11-03 19:43:09 +0200

    Data structures, Modern operators and Strings

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
 'use strict';
 /*
 
-JavaScript in the Browser: DOM and Events Fundamentals
+Data structures, Modern operators and Strings
 
 */
-
-// - Selecting and Manipulating Elements - //
-
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'JavaScript is fun';
-document.querySelector('.field').value = 1000000;
-
-// - Events listener -//
-
-// as the first argument, we had the name of the event that we're listening for, which is a click.
-
-// as the second argument, we have this function value.
-
-// this function will not be called immediately once the script here is executed. This function will only be called as soon as the event (click in this case) happens.
-
-document.querySelector('.btn').addEventListener('click', function () {
-  document.querySelector('.field').value = 0;
-});
-
-// best practice with elements is to select the elements that we need, and then we store the selections into variables. And then we can use them over and over again.
-//document.querySelector('.message').textContent = 'JavaScript is fun';
-
-const messageContent = function (message) {
-  document.querySelector('.message').textContent = message;
-};
-messageContent('I made some changes');
